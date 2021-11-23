d5f5b7bebba1e60f72b937d69311875701bf92be
Lemar Andar <lemarandar@outlook.com>
2021-11-23 16:54:24 -0500

    yuh

--- client/src/sprite/Skateboard.js
+++ client/src/sprite/Skateboard.js
@@ -1,14 +1,13 @@
 import TiledImage from "../TiledImage.js"
-import { leftArrowOn, rightArrowOn, ctx } from "../page-register.js"
+import { ctx } from "../page-register.js"
 
 export class Skateboard {
 	constructor() {
 		let columnCount = 11
 		let rowCount = 1
-		let delay = 50
+		let delay = 100
 		let loop = true
-		let scale = 0.5
-
+		let scale = 1
 
 		this.tiledImage = new TiledImage("img/sprites/rotating skateboard - 360.png", columnCount, rowCount, delay, loop, scale)
 
@@ -18,20 +17,8 @@ export class Skateboard {
 
 
 	tick () {
-		if (leftArrowOn){
-			this.x -= 2
-		}
-
-		if (rightArrowOn){
-			this.x += 2.5
-		}
-
-		if (!rightArrowOn && !leftArrowOn){
-			this.tiledImage.setPaused(true)
-		}
-		else{
-			this.tiledImage.setPaused(false)
-		}
+		
+		this.tiledImage.setPaused(false)
 
 		this.tiledImage.tick(this.x, this.y, ctx)
 		return true;
