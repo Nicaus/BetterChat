1d8e8eb9e9d35a4912805bbddd94fe1a4fc7ddbb
Lemar Andar <lemarandar@outlook.com>
2021-12-01 22:35:26 -0500

    commencement vue.js

--- client/src/Chat.vue
+++ client/src/Chat.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>asfdbjaksfb</div>
+    <div class="centered" style="color: black;">
+     {{ message }} 
+    </div>
 </template>
 
 <!-- La page chat doit utiliser la librarie Vue.js afin d'afficher les messages -->
@@ -7,12 +9,13 @@
 <!-- Pour la liste des membres connectés, vous pouvez utiliser Vue.js, ou le DOM.-->
 
 <script>
-export default {
-    name: "Chat",
-    data(){
-        return{
-
+import {newMessage} from './page-chat'
+    export default {
+        name: "Chat",
+        data(){
+            return{
+                message: newMessage
+            }
         }
     }
-}
 </script>
--- client/src/page-chat.js
+++ client/src/page-chat.js
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import Chat from './Chat.vue'
 import {registerCallbacks, sendMessage, signout, chatMessageLoop} from './chat-api';
 
-new Vue({
-    el: '#main',
-    components: { Chat },
-    template: '<Chat/>'
-})
-
 //- La page chat doit utiliser la librarie Vue.js afin d'afficher les messages 
 // (ceux qui arrivent dans la fonction newMessage du fichier chat.js). 
 // Pour la liste des membres connectés, vous pouvez utiliser Vue.js, ou le DOM.
@@ -19,15 +13,22 @@ window.addEventListener("load", () => {
     document.querySelector("#sign-out-btn").onclick = signout;
     registerCallbacks(newMessage, memberListUpdate);
     chatMessageLoop();
+
+
+    new Vue({
+        el: '#main',
+        components: { Chat },
+        template: '<Chat/>'
+    })
 })
 
 // Lorsqu'un nouveau message doit être affiché à l'écran, cette fonction est appelée
-const newMessage = (fromUser, message, isPrivate) => {
+export const newMessage = (fromUser, message, isPrivate) => {
     console.log(fromUser, message, isPrivate);
 }
 
 // À chaque 2-3 secondes, cette fonction est appelée. Il faudra donc mettre à jour la liste des membres
 // connectés dans votre interface.
-const memberListUpdate = members => {    
+const memberListUpdate = members => {
     console.log(members);
 }
